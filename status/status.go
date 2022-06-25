42557d4cdcd2304f95baba0212ad445e66295ce1
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-06-25 16:06:49 +0530

    Remove redundant status symbols

--- status/status.go
+++ status/status.go
@@ -6,25 +6,13 @@ import (
 )
 
 var (
-	// ℹ
-	Info = colors.Blue(symbols.Info)
+	Info = colors.Blue(symbols.Info) // ℹ
 
-	// ✔
-	Tick    = colors.Green(symbols.Tick)
-	Done    = colors.Green(symbols.Tick)
-	Success = colors.Green(symbols.Tick)
+	Tick = colors.Green(symbols.Tick) // ✔
 
-	// ✖
-	Error  = colors.Red(symbols.Cross)
-	Close  = colors.Red(symbols.Cross)
-	Cancel = colors.Red(symbols.Cross)
-	Cross  = colors.Red(symbols.Cross)
+	Cross = colors.Red(symbols.Cross) // ✖
 
-	// ▲
-	TriangleUp = colors.Green(symbols.TriangleUp)
-	Increase   = colors.Green(symbols.TriangleUp)
+	Increase = colors.Green(symbols.TriangleUp) // ▲
 
-	// ▼
-	TriangleDown = colors.Red(symbols.TriangleDown)
-	Decrease     = colors.Green(symbols.TriangleDown)
+	Decrease = colors.Green(symbols.TriangleDown) // ▼
 )
