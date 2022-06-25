90cd5190e67a3f19b6d7935643a3cb92e62d725e
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-06-25 18:34:03 +0530

    Move responsibility of splitting space inside Pad

--- format/pad.go
+++ format/pad.go
@@ -8,8 +8,12 @@ import "strings"
 
 //	Apply padding around the string
 func Pad(s, char string, count int) string {
-	space := strings.Repeat(char, count)
-	return space + s + space
+	space := count / 2
+	if space%2 == 0 {
+		return strings.Repeat(char, space) + s + strings.Repeat(char, space)
+	} else {
+		return strings.Repeat(char, space) + s + strings.Repeat(char, space+1)
+	}
 }
 
 //	Apply padding to the left of the string
