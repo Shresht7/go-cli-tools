f34dba6ce0a8c336c33fa7caaca0f2131e5883e3
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-08 15:42:20 +0530

    Create `determineMinIndentation` helper function

--- format/heredoc_test.go
+++ format/heredoc_test.go
@@ -36,3 +36,29 @@ func TestHereDoc(t *testing.T) {
 		}
 	}
 }
+
+func TestDetermineIndentationLevel(t *testing.T) {
+
+	// Setup test cases
+	cases := []struct {
+		in   []string
+		want int
+	}{
+		{[]string{}, 0},
+		{[]string{"Hello, World!"}, 0},
+		{[]string{" Hello, World!"}, 1},
+		{[]string{"  Hello, World!"}, 2},
+		{[]string{"\tHello, World!", "\t\tGO!"}, 1},
+		{[]string{"\t\tHello, World!", "\tGO!"}, 1},
+		{[]string{"\t\tHello, World!", "GO!"}, 0},
+	}
+
+	// Run test cases
+	for _, c := range cases {
+		got := determineMinIndentation(c.in)
+		if got != c.want {
+			t.Errorf("determineMinIndentation(%q) == %d, want %d", c.in, got, c.want)
+		}
+	}
+
+}
