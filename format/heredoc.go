8641259e7d1378e0c19b0a8e79e34b9c1fea0201
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2023-02-08 15:12:35 +0530

    Create `HereDoc` and `HereDocf` functions

---
+++ format/heredoc.go
@@ -0,0 +1,16 @@
+package format
+
+import "fmt"
+
+// HereDoc returns a here-document representation of the given string.
+// See https://en.m.wikipedia.org/wiki/Here_document for more information.
+func HereDoc(s string) string {
+	return s
+}
+
+// HereDocf returns a here-document (https://en.m.wikipedia.org/wiki/Here_document)
+// representation of the given string. The can be formatted using
+// the given arguments as in fmt.Sprintf.
+func HereDocf(format string, args ...any) string {
+	return fmt.Sprintf(format, args...)
+}
