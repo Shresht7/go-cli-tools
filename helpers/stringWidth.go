5def3f25bea27e68984626f6a08931c1a5cf4b71
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-01 23:02:08 +0530

    Minor refactor

--- helpers/stringWidth.go
+++ helpers/stringWidth.go
@@ -4,16 +4,18 @@ import (
 	"github.com/Shresht7/go-cli-tools/ansi/codes"
 )
 
+// Calculate the visual width of a string (the number of columns required to display it)
+// considering that ansi escape codes do not take up any columns.
 func StringWidth(s string) int {
 
+	// Strip ansi escape codes
 	s = codes.Strip(s)
 
-	//	TODO: Implementation Pending
+	// ? This does not handle characters that take up more than one column -
+	// ? For example, emojis, or characters from other languages. My goal for this
+	// ? project is to have zero dependencies, so the implementation for this is
+	// ? fairly simple.
 
-	width := 0
-	for range s {
-		width++
-	}
-
-	return width
+	// Return the length of the string
+	return len(s)
 }
