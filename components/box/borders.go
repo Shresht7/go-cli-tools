b329208c58d5ce600ecd0ab58c7b8c43d0b37fc2
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-01 23:33:53 +0530

    Add new box border styles

--- components/box/borders.go
+++ components/box/borders.go
@@ -25,3 +25,63 @@ var BorderSingle = BoxBorder{
 	left:        "│",
 	topLeft:     "┌",
 }
+
+// BorderDouble is a BoxBorder with double line characters
+// ╔════════╗
+// ║        ║
+// ╚════════╝
+var BorderDouble = BoxBorder{
+	top:         "═",
+	topRight:    "╗",
+	right:       "║",
+	bottomRight: "╝",
+	bottom:      "═",
+	bottomLeft:  "╚",
+	left:        "║",
+	topLeft:     "╔",
+}
+
+// BorderRound is a BoxBorder with round characters
+// ╭────────╮
+// │        │
+// ╰────────╯
+var BorderRound = BoxBorder{
+	top:         "─",
+	topRight:    "╮",
+	right:       "│",
+	bottomRight: "╯",
+	bottom:      "─",
+	bottomLeft:  "╰",
+	left:        "│",
+	topLeft:     "╭",
+}
+
+// BorderBold is a BoxBorder with bold characters
+// ┏━━━━━━━━┓
+// ┃        ┃
+// ┗━━━━━━━━┛
+var BorderBold = BoxBorder{
+	top:         "━",
+	topRight:    "┓",
+	right:       "┃",
+	bottomRight: "┛",
+	bottom:      "━",
+	bottomLeft:  "┗",
+	left:        "┃",
+	topLeft:     "┏",
+}
+
+// BorderClassic is a BoxBorder with classic characters
+// +--------+
+// |        |
+// +--------+
+var BorderClassic = BoxBorder{
+	top:         "-",
+	topRight:    "+",
+	right:       "|",
+	bottomRight: "+",
+	bottom:      "-",
+	bottomLeft:  "+",
+	left:        "|",
+	topLeft:     "+",
+}
