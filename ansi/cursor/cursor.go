0b67cb6e31d6dd4900ce2a2bd17e17c94230c5d5
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-02 16:21:24 +0530

    Implement `Save` and `Restore` functions

--- ansi/cursor/cursor.go
+++ ansi/cursor/cursor.go
@@ -6,56 +6,71 @@ import (
 	"github.com/Shresht7/go-cli-tools/ansi/codes"
 )
 
-//	Moves the cursor back to home position (0, 0)
+// Moves the cursor back to home position (0, 0)
 const ToHome = codes.CSI + "H"
 
-//	Moves the cursor back to the given row and column
+// Moves the cursor back to the given row and column
 func ToPos(row, column int) string {
 	return codes.CSI + fmt.Sprintf("%d;%dH", row, column)
 }
 
-//	Moves the cursor up by n number of lines
+// Moves the cursor up by n number of lines
 func Up(n int) string {
 	return codes.CSI + fmt.Sprintf("%dA", n)
 }
 
-//	Moves the cursor down by n number of lines
+// Moves the cursor down by n number of lines
 func Down(n int) string {
 	return codes.CSI + fmt.Sprintf("%dB", n)
 }
 
-//	Moves the cursor right by n number of lines
+// Moves the cursor right by n number of lines
 func Right(n int) string {
 	return codes.CSI + fmt.Sprintf("%dC", n)
 }
 
-//	Moves the cursor left by n number of lines
+// Moves the cursor left by n number of lines
 func Left(n int) string {
 	return codes.CSI + fmt.Sprintf("%dD", n)
 }
 
-//	Moves the cursor to the nth next line
+// Moves the cursor to the nth next line
 func ToNextLine(n int) string {
 	return codes.CSI + fmt.Sprintf("%dE", n)
 }
 
-//	Moves the cursor to the nth prev line
+// Moves the cursor to the nth prev line
 func ToPrevLine(n int) string {
 	return codes.CSI + fmt.Sprintf("%dF", n)
 }
 
-//	Moves the cursor to a given column position
+// Moves the cursor to a given column position
 func ToColumn(n int) string {
 	return codes.CSI + fmt.Sprintf("%dG", n)
 }
 
-//	Returns the current cursor position
+// Returns the current cursor position
 const RequestPosition = codes.CSI + "%6n"
 
-//	Show the cursor
+// Show the cursor
 const Show = codes.CSI + "?25h"
 
-//	Hide the cursor
+// Hide the cursor
 const Hide = codes.CSI + "?25l"
 
-//	? Save and Restore
+// Save saves the current cursor position.
+// The following commands have not been standardized and may have different functionality across different terminal applications. DEC Sequences are recommended.
+func Save(mode string) string {
+	if mode == "DEC" {
+		return codes.ESC + "7"
+	}
+	return codes.CSI + "s"
+}
+
+// Restore restores the current cursor position.
+func Restore(mode string) string {
+	if mode == "DEC" {
+		return codes.ESC + "8"
+	}
+	return codes.CSI + "u"
+}
